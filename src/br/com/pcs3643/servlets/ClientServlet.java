1ca164a6ba380d11b13bcc83938b35140c672598
Renata Brasil <renatabbss@gmail.com>
2017-10-15 23:04:21 -0200

    Adiciona a dependencia do pacote javax.servlet-api no pom.xml

--- src/br/com/pcs3643/servlets/ClientServlet.java
+++ src/br/com/pcs3643/servlets/ClientServlet.java
@@ -2,7 +2,6 @@ package br.com.pcs3643.servlets;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
